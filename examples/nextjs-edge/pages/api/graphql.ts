d7c25fdb78cf30c9bc53866585c6d5a18b594329
Laurin Quast <laurinquast@googlemail.com>
2023-02-24 11:30:17 +0100

    small documentation improvements

--- examples/nextjs-edge/pages/api/graphql.ts
+++ examples/nextjs-edge/pages/api/graphql.ts
@@ -1,6 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createYoga, createSchema } from 'graphql-yoga'
-import { NextRequest, NextFetchEvent } from 'next/server'
 
 // Docs: https://vercel.com/docs/concepts/functions/edge-functions
 
